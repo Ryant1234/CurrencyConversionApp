20b1429cca2d9f83a8bd90d72068e6f3fa043f1a
Ryan <Ryant1234@gmail.com>
2017-04-29 14:58:40 +1200

    .

---
+++ ExchangeRateApp/ExchangeRateApp/Models/ExchangeRate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateApp.Models
+{
+   public class ExchangeRate
+    {
+        public string @base { get; set; }
+        public string date { get; set; }
+        public Rates rates { get; set; }
+
+
+
+
+        public class Rates
+        {
+            public double AUD { get; set; }
+            public double CAD { get; set; }
+            public double CHF { get; set; }
+            public double CYP { get; set; }
+            public double CZK { get; set; }
+            public double DKK { get; set; }
+            public double EEK { get; set; }
+            public double GBP { get; set; }
+            public double HKD { get; set; }
+            public double HUF { get; set; }
+            public double ISK { get; set; }
+            public double JPY { get; set; }
+            public int KRW { get; set; }
+            public double LTL { get; set; }
+            public double LVL { get; set; }
+            public double MTL { get; set; }
+            public double NOK { get; set; }
+            public double NZD { get; set; }
+            public double PLN { get; set; }
+            public int ROL { get; set; }
+            public double SEK { get; set; }
+            public double SGD { get; set; }
+            public double SIT { get; set; }
+            public double SKK { get; set; }
+            public int TRL { get; set; }
+            public double USD { get; set; }
+            public double ZAR { get; set; }
+        }
+
+        
+      
+        
+    }
+}
---
+++ ExchangeRateApp/ExchangeRateApp/Services/ExchangeRateService.cs
@@ -0,0 +1,35 @@
+﻿using ExchangeRateApp.Models;
+using ModernHttpClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateApp.Services
+{
+   public  class ExchangeRateService
+    {
+        private const string Url = "http://api.fixer.io/latest?base=";
+
+        // NativeMessageHandler because of  HttpClient throwing exceptions..
+
+        private HttpClient _client = new HttpClient(new NativeMessageHandler());
+
+        public async Task<ExchangeRate> GetExchangeRate(string exchangeFrom)
+        {
+            // Gets a single movie for the detail page
+            var response = await _client.GetAsync($"{Url}");
+
+            // If response statuscode is equal to StatusCode NotFound
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return null;
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ExchangeRate>(content);
+        }
+    }
+}
+
