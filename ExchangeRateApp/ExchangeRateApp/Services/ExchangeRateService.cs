a124d1406a8b37df67e807721b8c0ec297cb3563
Ryan <Ryant1234@gmail.com>
2017-05-01 09:21:25 +1200

    Can now get an exchange rate back

--- ExchangeRateApp/ExchangeRateApp/Services/ExchangeRateService.cs
+++ ExchangeRateApp/ExchangeRateApp/Services/ExchangeRateService.cs
@@ -12,24 +12,117 @@ namespace ExchangeRateApp.Services
 {
    public  class ExchangeRateService
     {
+
+        List<String> countryNames = new List<string>();
+     
+
         private const string Url = "http://api.fixer.io/latest?base=";
 
         // NativeMessageHandler because of  HttpClient throwing exceptions..
 
         private HttpClient _client = new HttpClient(new NativeMessageHandler());
 
-        public async Task<ExchangeRate> GetExchangeRate(string exchangeFrom)
+
+        public async Task <ExchangeRate> GetExchangeRates(string exchangeFrom, string exchangeTo)
         {
+
+           var currencyFrom = countriesDictionary[exchangeFrom];
+            var currencyTo = countriesDictionary[exchangeTo];
+             
+
             // Gets a single movie for the detail page
-            var response = await _client.GetAsync($"{Url}");
+            var response = await _client.GetAsync($"{Url}{currencyFrom}");
 
             // If response statuscode is equal to StatusCode NotFound
             if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
                 return null;
 
             var content = await response.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<ExchangeRate>(content);
-        }
-    }
-}
+            return   JsonConvert.DeserializeObject<ExchangeRate>(content);
 
+           
+
+        }
+
+
+
+
+
+        public int CaculateExchangeRate(string exchangeFromAmount, int exchangeToRate)
+
+        {
+
+            
+
+                return 5;
+              
+        }
+
+
+
+
+
+     
+    
+
+
+    public Dictionary<string, string> countriesDictionary = new Dictionary<string, string>
+    {
+        {"Australian Dollar", "AUD"  },
+        {"Canadian Dollar",  "CAD" },
+        {"Swiss Franc",  "CHF" },
+        {"Czech Koruna", "CZK"  },
+        {"Danish Krone",  "DKK"  },
+        
+        {"Pound Sterling", "GBP"  },
+        {"Hong Kong Dollar",  "HKD"  },
+        {"Hungarian Forint", "HUF"  },
+        {"Iceland Krona", "ISK"  },
+        {"Japanese Yen",  "JPY"  },
+        {"Korean Won", "KRW"  },
+        {"Lithuanian Litas", "LTL"  },
+        {"Latvian Lats",  "LVL"  },
+        { "Norwegian Krone" , "NOK" },
+        { "New Zealand Dollar",  "NZD" },
+        {"Polish Zloty",  "PLN"  },
+        {"Swedish Krona" , "SEK" },
+        { "Singapore Dollar", "SGD"  },
+        { "US Dollar",  "USD" },
+        { "South African Rand",  "ZAR" },
+
+    };
+
+        public List<string> countriesCurrencyList = new List<string>
+        {
+            "Australian Dollar",
+            "Canadian Dollar",
+            "Swiss Franc",
+            "Czech Koruna",
+            "Danish Krone",
+            "Pound Sterling",
+            "Hong Kong Dollar",
+            "Hungarian Forint",
+            "Iceland Krona",
+            "Japanese Yen",
+            "Korean Won",
+            "Lithuanian Litas",
+            "Latvian Lats",
+            "Norwegian Krone" ,
+            "New Zealand Dollar",
+            "Polish Zloty",
+            "Swedish Krona" ,
+            "Singapore Dollar",
+            "US Dollar",
+            "South African Rand"
+        };
+
+
+
+    
+
+
+
+    }
+
+
+}
--- ExchangeRateApp/ExchangeRateApp/Views/ExchangeRatePage.xaml.cs
+++ ExchangeRateApp/ExchangeRateApp/Views/ExchangeRatePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExchangeRateApp.Services;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +11,53 @@ using Xamarin.Forms.Xaml;
 
 namespace ExchangeRateApp.Views
 {
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ExchangeRatePage : ContentPage
     {
+        private ExchangeRateService _exchangeRateService = new ExchangeRateService();
+
         public ExchangeRatePage()
         {
             InitializeComponent();
+            pickerConvertFrom.ItemsSource = _exchangeRateService.countriesCurrencyList;
+            pickerConvertTo.ItemsSource = _exchangeRateService.countriesCurrencyList;
+
         }
+
+        async void pickerConvertFrom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var currencyFrom = pickerConvertFrom.Items[pickerConvertFrom.SelectedIndex];
+            var currencyTo = pickerConvertTo.Items[pickerConvertTo.SelectedIndex];
+            var exchangeAmount = int.Parse(txtCurrencyAmount.Text);
+
+
+        
+                var exchangeRates = await _exchangeRateService.GetExchangeRates(currencyFrom, currencyTo, exchangeAmount);
+
+
+            var exchangeTo = _exchangeRateService.countriesDictionary[currencyTo];
+
+            var test = exchangeRates.rates[exchangeTo].ToString();
+
+         await   DisplayAlert("title", test, "cancel");
+               
+            }
+      
+         
+
+
+        
+
+
+        }
+
+
+
+
+          
+
+
+
+    
     }
-}
